fa8b2aeddd87232235e4e5c5e8bedda03f8e5282
Tony Tiger <thatrascaltiger@gmail.com>
2016-07-22 10:29:25 +0100

    Adding basic testing of the report handler
    This would be a good time now for me to complain a little bit about the
    complete lack of documentation and infrastructure around unit and integration
    testing under the serverless framework. I've found a testing harness which
    appears to be useful, but there is no clear way to me to be able to have
    separate, clear unit tests on board without having to create more npm modules
    and including them as more dependencies.

--- functions/receive_csp_report/handler.js
+++ functions/receive_csp_report/handler.js
@@ -1,11 +1,22 @@
 'use strict';
 
-var AWS = require('aws-sdk');
-var s3  = new AWS.S3({ params: { Bucket: process.env.S3_BUCKET_NAME } });
+// Bootstrap for local testing
+var bucket
+if(process.env.SERVERLESS_TEST) {
+  var AWSMock = require('mock-aws-s3');
+  var s3 = AWSMock.S3({
+    params: { Bucket: 'receive_csp_report' }
+  });
+} else {
+  var AWS = require('aws-sdk');
+  var s3  = new AWS.S3({ params: { Bucket: process.env.S3_BUCKET_NAME } });
+}
+
 
 module.exports.handler = function(event, context, cb) {
 
   event.headers = JSON.parse(event.headers);
+
   var payload = {
     Key: event.requestId,
     Body: JSON.stringify(event),
